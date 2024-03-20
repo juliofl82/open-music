927ba45d2e62daf8508c4d7ab615117247cf77fa
juliofl82 <jfigueiredolamounier@gmail.com>
2024-03-19 22:40:37 -0300

    update
    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,4 +5,27 @@
 
 
 
+// A função para selecionar um gênero e destacar o botão correspondente
+function selectGenre(selectedButton) {
+    // Encontra todos os botões na lista
+    const buttons = document.querySelectorAll('.filterByGenre_button');
+
+    // Remove a classe 'active' de todos os botões
+    buttons.forEach(button => {
+        button.classList.remove('active');
+    });
+
+    // Adiciona a classe 'active' ao botão que foi clicado
+    selectedButton.classList.add('active');
+}
+
+// Adiciona o evento de clique a todos os botões
+document.querySelectorAll('.filterByGenre_button').forEach(button => {
+    button.addEventListener('click', function() {
+        selectGenre(this);
+    });
+});
+
+
+
 
